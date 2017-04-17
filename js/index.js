fbe33fe39345111a5051404ac9fbd31cd06542ce
2506738200@qq.com <xin360360>
2017-04-17 19:18:35 +0800

    'vuedemo'

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+// 
+var exampleData={
+	message:'hello world'
+}
+
+new Vue({
+	el: '#app',
+	data: exampleData
+})
+
+/*var exampleData={
+	name:'Vue.js'
+}
+
+var exampleVM= new Vue({
+	el: '#example',
+	data: exampleData
+})*/
